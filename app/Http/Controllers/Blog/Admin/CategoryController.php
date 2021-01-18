0c3b44d1fbda7ff1932d3bdc5949ebb170b51509
Igor Sharendo <axe_dream@list.ru>
2021-01-18 14:29:40 +0700

    валидация и сохранение

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Blog\Admin;
 use App\Models\BlogCategory;
 use Illuminate\Http\Request;
+use App\Http\Requests\BlogCategoryUpdateRequest;
 
 class CategoryController extends BaseController
 {
@@ -15,7 +16,7 @@ class CategoryController extends BaseController
     {
         $paginator = BlogCategory::paginate(5);
 
-        return view('blog.admin.category.index',compact('paginator'));
+        return view('blog.admin.categories.index',compact('paginator'));
     }
 
     /**
@@ -49,18 +50,54 @@ class CategoryController extends BaseController
     {
         $item = BlogCategory::findOrFail($id);
         $categoryList = BlogCategory::all();
-        return view('blog.admin.category.edit',compact('item','categoryList'));
+        return view('blog.admin.categories.edit',compact('item','categoryList'));
     }
 
+
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
-    public function update(Request $request, $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
+        /*
+            $rules = [
+                'title' => 'required|min:5|max:200',
+                'slug'  =>  'max:200',
+                'description' => 'string|min:3|max:500',
+                'parent_id' => 'required|integer|exists:blog_categories,id',
+            ];
+
+            //$validateData = $this->validate($request,$rules);
+
+            //$validateData = $request->validate($rules);   //редирект в случае ошибки
+
+
+            $validator = \Validator::make($request->all(),$rules);
+            $validateData[] = $validator->passes();     //и выполнит проверку, заполняет даные ошибок, вернет TRUE если ОК, и FALSE если НЕ ОК и есть ошибки
+            $validateData[] = $validator->validate(); //редиректит если данные не валидны
+            $validateData[] = $validator->valid();      // те поля которые валидны
+            $validateData[] = $validator->failed();     // те поля которые не валидны
+            $validateData[] = $validator->errors();     // сообщения о ошибках
+            $validateData[] = $validator->fails();      // TRUE = если ошибки, FALSE = нет ошибок
+
+            dd($validateData);
+        */
+
+        $item = BlogCategory::find($id);
+        if (empty($item)) {
+            return back()->withErrors(['msg'=>"Запись ID-[{$id}] не найдена"])->withInput();
+        }
+
+        $data = $request->all();
+        $result = $item->fill($data)->save();
+        if ($result) {
+            return redirect()->route('blog.admin.categories.edit',$item->id)->with(['success'=> 'Успешно сохранено']);
+        } else {
+            return back()->withErrors(['msg'=>'Ошибка сохранения'])->withInput();
+        }
 
     }
 
--- resources/views/blog/admin/categories/includes/item_edit_main_col.blade.php
+++ resources/views/blog/admin/categories/includes/item_edit_main_col.blade.php
@@ -53,8 +53,7 @@
                             <textarea name="description"
                                       id="description"
                                       class="form-control"
-                                      row="3">
-                                {{$item->description}}
+                                      row="3">{{old('description',$item->description)}}
                             </textarea>
                         </div>
 
